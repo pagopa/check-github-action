1d95d1b205b437455b80f316d675ec082d29aa95
Jacopo <jacopo.carlini@gmail.com>
2022-12-22 17:57:06 +0100

    fix

--- com/jacopo/Main.java
+++ com/jacopo/Main.java
@@ -1,10 +1,12 @@
 package com.jacopo;
 
 public class Main {
+
+
     public static void main(String[] args) {
-        int a =0;if(a==0){}
-        else{
+        int a = 0;if (a == 0) {
+        } else {
         }
     }
-
+//sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
 }
