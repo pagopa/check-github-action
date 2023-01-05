a77a0a87931feaefda18ef110b592c5c04796aa7
Jacopo <jacopo.carlini@gmail.com>
2023-01-05 15:05:29 +0100

    new

--- src/main/java/it/gov/pagopa/afm/calculator/repository/ValidBundleRepository.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.it.gov.pagopa.afm.calculator.repository;
-
-import it.gov.pagopa.afm.calculator.entity.ValidBundle;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ValidBundleRepository extends CosmosRepository<ValidBundle, String> {}
